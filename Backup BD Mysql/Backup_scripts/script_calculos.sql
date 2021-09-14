047888f49a1cb9c6c150dad84c6a790e9da0f626
João Marcos Africo da Silva <65793821+JMAfrico@users.noreply.github.com>
2021-09-14 08:41:16 -0300

    Delete script_calculos.sql

--- Backup BD Mysql/Backup_scripts/script_calculos.sql
+++
@@ -1,3 +0,0 @@
-select * from tb_bebida;
-select sum(preco_bebida)from tb_bebida;/*soma o preço de todas as bebidas cadastradas*/
-select count(cod_bebida) from tb_bebida;/*conta a quantidade de bebidas cadastradas*/
